eaf0fc1361408aee282474c37b99a10793e9f859
Tich Zvidzayi <tzvidzie@gmail.com>
2024-03-30 02:31:26 +0200

    Add_custom_hook:useFetchAPI

--- alphavantage/src/App.tsx
+++ alphavantage/src/App.tsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useApiFetch from './customhooks/useAPIFetch';
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const apiKey = 'RIBXT3XYLI69PC0Q';
+const apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
+
+const {data, isLoading, error} = useApiFetch(apiKey, apiUrl);
+
+const response = JSON.stringify(data, null, 2);
+console.log("Hey.. => " + response);
+
+if (isLoading)
+{
+
+  return <div> Loading content...</div>
+}
+
+if (error)
+{
+
+  return <div> Ooops an error occurred. : {error}</div>
+}
+
+const metaData = data?.['Meta Data'];
+const  timeSeries = data?.['Time Series (5min)'];
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
+    <div className="grid grid-cols-2 gap-4 bg-blue-600 ">
+      {metaData && (
+        <div className='bg-blue-500 '>
+          <h2 className="text-lg font-bold mb-2">{metaData['2. Symbol']}</h2>
+          <p>{metaData['1. Information']}</p>
+          <p>Last Refreshed: {metaData['3. Last Refreshed']}</p>
+          <p>Interval: {metaData['4. Interval']}</p>
+          <p>Time Zone: {metaData['6. Time Zone']}</p>
+        </div>
+      )}
+
+      {timeSeries && (
+        <div className="col-span-2">
+          <h3 className="text-md font-bold mb-2">Time Series (5min)</h3>
+          <ul>
+            {Object.entries(timeSeries).map(([timestamp, values]) => (
+              <li key={timestamp} className="grid grid-cols-2 gap-2">
+                <strong>{timestamp}</strong>
+                <div>
+                  <p>Open: {values['1. open']}</p>
+                  <p>High: {values['2. high']}</p>
+                  <p>Low: {values['3. low']}</p>
+                  <p>Close: {values['4. close']}</p>
+                  <p>Volume: {values['5. volume']}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   )
 }
 
